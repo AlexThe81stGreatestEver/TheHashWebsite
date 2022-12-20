148c5fcfb7ddad9b578c63316663936e878580d0
Goes Down Dirty <goesdowndirty@gmail.com>
2022-12-19 21:12:51 -0500

    Remove RoutingServiceProvider.php.

--- index.php
+++ index.php
@@ -11,12 +11,14 @@ require_once 'HASH/Controller/AdminController.php';
 require_once 'HASH/Controller/SuperAdminController.php';
 require_once 'HASH/Controller/ObscureStatisticsController.php';
 require_once 'HASH/UserProvider.php';
-require_once 'Provider/RoutingServiceProvider.php';
 require_once 'Provider/HttpKernelServiceProvider.php';
 require_once 'Provider/EventListenerProvider.php';
 require_once 'Provider/SecurityServiceProvider.php';
 require_once 'Provider/MonologServiceProvider.php';
 require_once 'Application.php';
+require_once 'ControllerCollection.php';
+require_once 'Provider/Routing/RedirectableUrlMatcher.php';
+require_once 'Provider/Routing/LazyRequestMatcher.php';
 require_once 'Psr11ServiceProvider.php';
 
 use Doctrine\DBAL\Schema\Table;
@@ -61,7 +63,12 @@ use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\HttpFoundation\UrlHelper;
 use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\EventListener\SessionListener;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Security\Core\Encoder\EncoderFactory;
 use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -101,7 +108,57 @@ if($app['debug']) {
 }
 
 $app->register(new Provider\HttpKernelServiceProvider());
-$app->register(new Provider\RoutingServiceProvider());
+
+$app['route_class'] = 'Symfony\Component\Routing\Route';
+
+$app['request_context'] = function ($app) {
+    $context = new RequestContext();
+
+    $context->setHttpPort(isset($app['request.http_port']) ? $app['request.http_port'] : 80);
+    $context->setHttpsPort(isset($app['request.https_port']) ? $app['request.https_port'] : 443);
+
+    return $context;
+};
+
+$app['route_factory'] = $app->factory(function ($app) {
+    return new $app['route_class']('/');
+});
+
+$app['routes_factory'] = $app->factory(function () {
+    return new RouteCollection();
+});
+
+$app['routes'] = function ($app) {
+    return $app['routes_factory'];
+};
+
+$app['url_generator'] = function ($app) {
+    return new UrlGenerator($app['routes'], $app['request_context']);
+};
+
+$app['request_matcher'] = function ($app) {
+    return new Provider\Routing\RedirectableUrlMatcher($app['routes'], $app['request_context']);
+};
+
+$app['controllers'] = function ($app) {
+    return $app['controllers_factory'];
+};
+
+$controllers_factory = function () use ($app, &$controllers_factory) {
+    return new \ControllerCollection($app['route_factory'], $app['routes_factory'], $controllers_factory);
+};
+$app['controllers_factory'] = $app->factory($controllers_factory);
+
+$app['routing.listener'] = function ($app) {
+    $urlMatcher = new Provider\Routing\LazyRequestMatcher(function () use ($app) {
+        return $app['request_matcher'];
+    });
+
+    return new RouterListener($urlMatcher, $app['request_stack'], $app['request_context'], $app['logger'], null, isset($app['debug']) ? $app['debug'] : false);
+};
+
+$app['dispatcher']->addSubscriber($app['routing.listener']);
+
 $app->register(new Psr11ServiceProvider());
 
 $app->extend('resolver', function ($resolver, $app) {
